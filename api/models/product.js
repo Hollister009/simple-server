93aaeefc5d9827a2c146fbc9ddcbff5100fa1174
Hollister009 <hipnos.az@gmail.com>
2019-10-14 15:03:27 +0300

    Updated product Schema according to new Product fields

--- api/models/product.js
+++ api/models/product.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
 
+const imageSchema = new mongoose.Schema({
+  value: String,
+  urls: [String]
+});
+
 const productSchema = new mongoose.Schema({
   productName: String,
   description: String,
-  colors: [{
-    value: String,
-    url: String
-  }],
   brand: String,
   sizes: [String],
+  colors: [String],
+  images: [imageSchema],
+  sex: String,
   category: String,
+  season: [String],
   quantity: Number,
   price: Number,
-})
+  video: {
+    type: String,
+    required: false
+  }
+});
+
 
 module.exports = mongoose.model('Product', productSchema);
