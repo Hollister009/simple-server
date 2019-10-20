01ce843338697cf4927d307704d6e709fd5ece50
Nazar-bnm <nazar.bnm@gmail.com>
2019-10-20 13:08:39 +0300

    add brands endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controllers = require('../controllers');
 const { getMockedProducts, createProduct, getAllProducts, findProductById, getProduct } = controllers;
+const { getBrands } = require('../controllers/brands.controller');
 
 router.get('/', (req, res) => {
   res.send('Server works fine!');
@@ -10,5 +11,7 @@ router.post('/products', createProduct);
 router.get('/products', getAllProducts);
 router.get('/products/mock', getMockedProducts);
 router.get('/products/:id', findProductById, getProduct);
+
+router.get('/brands', getBrands);
 
 module.exports = router;
