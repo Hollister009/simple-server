af09f16645638285c68e6950123d6c554564228a
Nazar-bnm <nazar.bnm@gmail.com>
2019-10-22 17:09:16 +0300

    add brands

--- api/controllers/brands.controller.js
+++ api/controllers/brands.controller.js
@@ -13,6 +13,19 @@ const getBrands = async (req, res) => {
   }
 }
 
+const createBrand = async (req, res) => {
+  const { title, logo } = req.body;
+  const brand = new Brands({ title, logo });
+
+  try {
+    const response = await brand.save();
+    res.status(201).json(response);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
 module.exports = {
-  getBrands
+  getBrands,
+  createBrand
 };
