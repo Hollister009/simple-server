f88ca23206d22dc1cb8da3edf01518a6aa0311b7
Hollister009 <hipnos.az@gmail.com>
2019-10-24 02:41:06 +0300

    added swagger modules and created base documentation for products route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+// swagger modules
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const {
   getProduct,
   createProduct,
@@ -11,14 +14,72 @@ const {
 const { getBrands, createBrand } = require('../controllers/brands.controller');
 const { createImage } = require('../controllers/images.controller');
 
+// Extended: https://swagger.io/specification/#infoObject
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Simple REST API Server',
+      version: '1.0.0',
+      description: 'A sample API'
+    },
+    basePath: '/'
+  },
+  apis: ['./api/routes/*.js']
+};
+const swaggerSpec = swaggerJSDoc(options);
+
 router.get('/', (req, res) => {
-  res.send('Server works fine!');
+  res.send('<h2>Server works fine!</h2>');
 });
 
-router.route('/products')
-  .post(createProduct)
-  .get(getAllProducts)
-  .delete(removeAllProducts);
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /products:
+ *  get:
+ *    description: Read list of all products
+ *    responses:
+ *      '200':
+ *        description: Return array of products
+ *      '500':
+ *        description: Error message
+ */
+router.get('/products', getAllProducts);
+
+/**
+ * @swagger
+ * /products:
+ *  post:
+ *    description: Create new product
+ *    parameters:
+ *      - name: body
+ *        description: Created product object
+ *        required: true
+ *        type: object
+ *        example: TODO
+ *    responses:
+ *      '201':
+ *        description: Return newly created product
+ *      '400':
+ *        description: Error message
+ */
+router.post('/products', createProduct);
+
+/**
+ * @swagger
+ * /products:
+ *  delete:
+ *    description: Delete all products
+ *    responses:
+ *      '200':
+ *        description: Successful message
+ *      '400':
+ *        description: Error message
+ */
+router.delete('/products', removeAllProducts);
+
   
 router.get('/products/mock', getMockedProducts);
 router.get('/products/:id', findProductById, getProduct);
