dce714809a77e2bbb0f5004f7f0cd343bf0d72f9
Hollister009 <hipnos.az@gmail.com>
2019-11-19 11:59:11 +0200

    Pino logger added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const cors = require('cors');
+const pino = require('pino');
 const express = require('express');
-const morgan = require('morgan');
+const expressPino = require('express-pino-logger');
 // mongodb connection & env variables
 process.env.NODE_ENV !== 'production' && require('dotenv').config();
 require('./db');
@@ -12,11 +13,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const staticDir = path.resolve('static');
 
+const logger = pino();
+const expressLogger = expressPino({ logger });
+
 app.use(cors())
   .use(express.json())
   .use(express.static(staticDir))
   .use(express.urlencoded({ extended:false }))  
-  .use(morgan('combined'))
+  .use(expressLogger)
   .use(router);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => {
+  logger.info('Server is running on port %d', port)
+});
